fe5db7495046450f5fed262ad1fee63b82be68cd
grgsiberia <nanashi4129@gmail.com>
2023-10-23 17:22:45 +0900

    画面設計をする

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from kivy.lang import Builder
 from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.resources import resource_add_path
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.spinner import Spinner
 
 class MainScreen(BoxLayout):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
+
+class SortSpinner(Spinner):
+    pass
+
+class SortTypeSpinner(Spinner):
+    pass
 
 class ViewerApp(App):
     def build(self):
