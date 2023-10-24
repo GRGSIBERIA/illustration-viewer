d2e975d3981dc47ce3b7dfe9bd668569c2d7c2b3
grgsiberia <nanashi4129@gmail.com>
2023-10-24 15:04:55 +0900

    タグ名から画像を検索する

---
+++ src/sql/search_tags_in_pictures.sql
@@ -0,0 +1,24 @@
+select
+    I.rowid,
+    I.created_at,
+    I.imported_at,
+    I.is_star,
+    I.goods,
+    I.imported_path,
+    P.item,
+    P.width,
+    P.height,
+    TM.item,
+    TM.width,
+    TM.height
+from informations as I
+    inner join assign_info_tags as IT on IT.info_id = I.rowid
+    inner join tags as T on IT.tag_id = T.rowid
+    inner join pictures as P on I.picture_id = P.rowid
+    inner join thumbs as TM on I.thumb_id = P.rowid
+where T.name in (???)
+    order by P.rowid ?
+    LIMIT ? OFFSET ?;
+
+/* where句でin句を使う場合、配列を指定できないため、プレースホルダーの?をその個数だけ置き換える */
+/* [A, B, C] ならば replace("???", "?, ?, ?")
