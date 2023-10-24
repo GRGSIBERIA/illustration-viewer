2af6cc43f1d9376485041dc691be9c43a638a507
grgsiberia <nanashi4129@gmail.com>
2023-10-24 13:51:02 +0900

    画像一覧を取得する

---
+++ src/sql/get_pictures.sql
@@ -0,0 +1,17 @@
+select 
+    I.created_at,
+    I.imported_at,
+    I.is_star,
+    I.goods,
+    I.imported_path,
+    P.item,
+    P.width,
+    P.height,
+    T.item,
+    T.width,
+    T.height
+from informations as I
+    inner join pictures as P on I.picture_id = P.rowid
+    inner join thumbs as T on I.thumb_id = T.rowid
+    order by I.rowid ?
+    LIMIT ? OFFSET ?;
