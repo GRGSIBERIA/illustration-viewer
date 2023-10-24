90110a2608997ad1a6c0c72cd1ea0192e8692652
grgsiberia <nanashi4129@gmail.com>
2023-10-24 13:20:01 +0900

    vacuumの入れ忘れ

--- src/sql/drop.sql
+++ src/sql/drop.sql
@@ -3,3 +3,4 @@ drop index if exists tags_name_unique;
 drop index if exists assign_info_index, assign_tag_index;
 drop index if exists albums_info_index, albums_name_index, albums_created_index;
 drop table if exists pictures, thumbs, informations, tags, assign_info_tags, albums;
+VACUUM;
