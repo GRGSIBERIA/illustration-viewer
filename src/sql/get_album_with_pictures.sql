bfa98c5e3de3e06b6aa45215d1bf1e3a0bcb6966
grgsiberia <nanashi4129@gmail.com>
2023-10-24 14:00:02 +0900

    アルバムにぶら下がっている画像を取得する

---
+++ src/sql/get_album_with_pictures.sql
@@ -0,0 +1,19 @@
+select
+    A.name,
+    I.created_at,
+    I.imported_at,
+    I.is_star,
+    I.goods,
+    I.imported_path,
+    P.item,
+    P.width,
+    P.height,
+    T.item,
+    T.width,
+    T.height
+from albums as A
+    inner join informations as I on A.info_id = I.rowid
+    inner join pictures as P on P.rowid = I.picture_id
+    inner join thumbs as T on T.rowid = I.picture_id
+    order by I.rowid ?
+    LIMIT ? OFFSET ?;
