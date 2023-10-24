cd388c54bf5b56840e69649ea3a7914a8544d9ac
grgsiberia <nanashi4129@gmail.com>
2023-10-24 15:46:36 +0900

    whereで名前を指定するのを忘れた

--- src/sql/search_tags_on_parent.sql
+++ src/sql/search_tags_on_parent.sql
@@ -3,4 +3,6 @@ select
     A.name,
     A.parent_id
 from tags as A
-    inner join tags as B on A.parent_id = B.rowid;
+    inner join tags as B on A.parent_id = B.rowid
+where B.name = :name
+    order by :target :orderby;
