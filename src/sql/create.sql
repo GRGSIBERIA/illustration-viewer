02d23d8ac16e0a4182a941e717ddd3f3db5212ff
grgsiberia <nanashi4129@gmail.com>
2023-10-25 12:34:56 +0900

    これも2つ同時に使うのでインデックスを張っておく

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -58,10 +58,8 @@ create table if not exists assign_info_tags(
     created_at text NOT NULL default (DATETIME('now', 'localtime')),
     unique(info_id, tag_id)
 );
-create index if not exists assign_info_index
-    on assign_info_index on assign_info_tags(info_id);
-create index if not exists assign_tag_index
-    on assign_tag_index on assign_info_tags(tag_id);
+create unique index if not exists assign_info_tags_index
+    on assign_info_index on assign_info_tags(info_id, tag_id);
 create index if not exists assign_created_at_index
     on assign_tag_index on assign_info_tags(created_at);
 
