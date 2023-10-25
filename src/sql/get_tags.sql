5e7ed0d180a7bad178c5f1b03264c69d3ff2ade6
grgsiberia <nanashi4129@gmail.com>
2023-10-25 11:57:11 +0900

    distinctで複数の同じ結果を除外する

--- src/sql/get_tags.sql
+++ src/sql/get_tags.sql
@@ -1,6 +1,9 @@
 SELECT
-    rowid,
-    name,
-    parent_id
-from tags
+    distinct TAG.rowid,
+    TAG.name,
+    TAG.parent_id,
+    TAG.created_at,
+    count(AIT.tag_id) as item_count
+from tags as TAG
+    inner join assign_info_tags as AIT on TAG.rowid = AIT.tag_id
     order by :target :orderby;
