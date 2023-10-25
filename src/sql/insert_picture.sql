db191e3028daf5f23dd86b065cebe6f9c0c7b272
grgsiberia <nanashi4129@gmail.com>
2023-10-25 13:33:55 +0900

    コメントを追加

--- src/sql/insert_picture.sql
+++ src/sql/insert_picture.sql
@@ -1,3 +1,16 @@
+/*
+    画像を1件保存する
+    @input
+        picitem 画像
+        picsha1 画像のSHA1
+        picwidth 画像の幅
+        picheight 画像の高さ
+        thumitem サムネ
+        thumsha1 サムネのSHA1
+        thumwidth サムネの幅
+        thumheight サムネの高さ
+*/
+
 BEGIN IMMEDIATE;
 insert into pictures values (:picitem, :picsha1, :picwidth, :picheight);
 insert into thumbs values (:thumitem, :thumsha1, :thumwidth, :thumheight);
