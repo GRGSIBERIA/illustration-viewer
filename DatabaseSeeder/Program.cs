40d7782eecc662ef2d41e7edc99ef65bac0b5d9a
GRGSIBERIA <nanashi4129@gmail.com>
2023-10-28 16:05:50 +0900

    foreign key対応

--- DatabaseSeeder/Program.cs
+++ DatabaseSeeder/Program.cs
@@ -3,25 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 using System.Data.SQLite;
-
 
 namespace DatabaseSeeder
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void GenerateDatabase()
         {
             using (var conn = new SQLiteConnection("Data Source=mydb.sqlite"))
             {
                 conn.Open();
 
+                string sql = "";
+                using (var reader = new StreamReader("./Sqls/create.sql"))
+                {
+                    sql = reader.ReadToEnd();
+                }
+                Console.WriteLine(sql);
+
+                using (var command = new SQLiteCommand(sql, conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+
                 conn.Close();
+            }
+        }
+
+        static void Seeding()
+        {
+
+        }
+
+        static void DropDatabase()
+        {
+            using (var conn = new SQLiteConnection("Data Source=mydb.sqlite"))
+            {
+                conn.Open();
+
+                string sql = "";
+                using (var reader = new StreamReader("./Sqls/drop.sql"))
+                {
+                    sql = reader.ReadToEnd();
+                }
+                Console.WriteLine(sql);
+
+                using (var command = new SQLiteCommand(sql, conn))
+                {
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch(System.Data.SQLite.SQLiteException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                conn.Close();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("モードを切り替えます");
+            Console.WriteLine("1. データベース作成");
+            Console.WriteLine("2. シード");
+            Console.WriteLine("3. データベースをdropする");
+            var s = Console.ReadLine();
+            
+            switch (s)
+            {
+                case "1":
+                    GenerateDatabase();
+                    break;
+                case "2":
+                    Seeding();
+                    break;
+                case "3":
+                    DropDatabase();
+                    break;
+                default:
+                    Console.WriteLine("終了します");
+                    break;
             }
 
             Console.WriteLine("プログラムが修了しました");
-            Console.ReadLine();
+            Environment.Exit(0);
         }
     }
 }
--- DatabaseSeeder/Sqls/create.sql
+++ DatabaseSeeder/Sqls/create.sql
@@ -2,58 +2,62 @@
 
 /* 画像 */
 create table if not exists pictures (
+	id integer primary key autoincrement not null,
 	picture blob not null,
 	thumbnail blob not null,
-	sha1 text not null
+	sha1 text not null,
 	width integer not null,
 	height integer not null,
 	import_path text not null,
 	saved_at text not null default (DATETIME('now', 'localtime')),
 	created_at text not null
 );
-create unique index if not exists pictures_idpk_idx on pictures(rowid);
-create index if not exists pictures_savedid_idx on pictures(rowid, saved_at);
-create index if not exists pictures_createdid_idx on pictures(rowid, created_at);
+create unique index if not exists pictures_idpk_idx on pictures(id);
+create index if not exists pictures_savedid_idx on pictures(id, saved_at);
+create index if not exists pictures_createdid_idx on pictures(id, created_at);
 create index if not exists pictures_sha1_idx on pictures(sha1);
 
 
 /* タグ */
 create table if not exists tags (
+	id integer primary key autoincrement not null,
 	name text not null unique,
 	parent_id integer default NULL references tags(id)
 		on delete cascade on update cascade
 );
-create unique index if not exists tags_idname_idx on tags(rowid, name);
-create unique index if not exists tags_idparent_idx on tags(rowid, parent_id);
+create unique index if not exists tags_idname_idx on tags(id, name);
+create unique index if not exists tags_idparent_idx on tags(id, parent_id);
 
 
 /* アルバム */
 create table if not exists albums (
+	id integer primary key autoincrement not null,
 	name text not null unique
 );
-create unique index if not exists albums_idname_idx on albums(rowid, name);
+create unique index if not exists albums_idname_idx on albums(id, name);
 
 
 /* コレクション */
 create table if not exists collections (
+	id integer primary key autoincrement not null,
 	name text not null unique
 );
-create unique index if not exists collections_idname_idx on collections(rowid, name);
+create unique index if not exists collections_idname_idx on collections(id, name);
 
 
 /* タグと画像のアサイン */
 create table if not exists tag2pic (
-	tag_id integer not null references tags(rowid),
-	pic_id integer not null references pictures(rowid)
+	tag_id integer not null references tags(id),
+	pic_id integer not null references pictures(id)
 );
 create unique index if not exists tag2pic_tag_pic_idx on tag2pic(tag_id, pic_id);
 
 
 /* アルバムと画像のアサイン */
 create table if not exists album2pic (
-	album_id integer not null references albums(rowid)
+	album_id integer not null references albums(id)
 		on delete cascade on update cascade,
-	pic_id integer not null references pictures(rowid)
+	pic_id integer not null references pictures(id)
 		on delete cascade on update cascade
 );
 create unique index if not exists album2pic_album_pic_idx on album2pic(album_id, pic_id);
@@ -61,9 +65,9 @@ create unique index if not exists album2pic_album_pic_idx on album2pic(album_id,
 
 /* コレクションと画像のアサイン */
 create table if not exists col2pic (
-	col_id integer not null references collections(rowid)
+	col_id integer not null references collections(id)
 		on delete cascade on update cascade,
-	pic_id integer not null references pictures(rowid)
+	pic_id integer not null references pictures(id)
 		on delete cascade on update cascade
 );
 create unique index if not exists col2pic_col_pic_idx on col2pic(col_id, pic_id);
