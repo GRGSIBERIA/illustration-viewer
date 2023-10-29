c657ec518c61cb4af600035ad92f9926819e67a4
GRGSIBERIA <nanashi4129@gmail.com>
2023-10-29 15:15:04 +0900

    SQLを追加する

---
+++ DatabaseSeeder/Sqls/insert_tag.sql
@@ -0,0 +1,3 @@
+﻿insert into tags(name)
+select name from tags where not exists
+	(select 1 from tags where name = @name);
---
+++ DatabaseSeeder/Sqls/insert_tag2pic_with_name.sql
@@ -0,0 +1,3 @@
+﻿insert into tag2pic(tag_id, pic_id)
+select tags.id, @pic_id from tags
+where tags.name = @name;
