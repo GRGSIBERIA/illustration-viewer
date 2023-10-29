9d1c1989a163616c1eea76eb3cf3f924b878feaf
GRGSIBERIA <nanashi4129@gmail.com>
2023-10-29 12:42:36 +0900

    画像を挿入するためのクエリを書く

---
+++ DatabaseSeeder/Sqls/insert_picture.sql
@@ -0,0 +1,6 @@
+﻿insert into pictures(picture, thumbnail, sha1, ext, width, height, import_path, created_at, saved_at)
+	select @picture, @thumbnail, @sha1, @ext, @width, @height, @import_path, @created_at, @saved_at
+	from pictures where not exists (
+		select 1 from pictures
+		where sha1 = @sha1
+	);
